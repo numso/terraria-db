1e51a3019b9d65e232d5f1ad4f639b723a9bf8ef
Dallin Osmun <dosmun@gmail.com>
2022-01-14 15:34:47 -0700

    get something showing up

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,41 @@
-export default function Index () {
+import { useLoaderData } from 'remix'
+
+import recipes from '../../data/recipes.json'
+
+export const loader = () => {
+  return recipes
+}
+
+const compare = (a, b) => {
+  if (!a || !b) return false
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
+    a.toLowerCase().includes(b.toLowerCase()) ||
+    b.toLowerCase().includes(a.toLowerCase())
+  )
+}
+
+export default function Index () {
+  const [text, setText] = React.useState('')
+  const [selected, setSelected] = React.useState(null)
+  const recipes = useLoaderData()
+  const matches = recipes.filter(recipe => compare(recipe.item, text))
+  if (matches.length > 15) matches.length = 15
+  return (
+    <div>
+      <h1>Terraria Recipes</h1>
+      <label>Search</label>
+      <input
+        type='search'
+        className='outline outline-1 py-2 px-3'
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
       <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
+        {matches.map(match => (
+          <li onClick={() => setSelected(match)}>{match.item}</li>
+        ))}
       </ul>
+      {selected && <pre>{JSON.stringify(selected, null, 2)}</pre>}
     </div>
   )
 }

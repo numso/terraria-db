c162a59189e9369f8ecdebcf0fa70fb126c8240b
Dallin Osmun <dosmun@gmail.com>
2022-01-17 09:04:55 -0700

    looks good now

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import _ from 'lodash'
 import { nanoid } from 'nanoid'
 import { useLoaderData } from 'remix'
@@ -17,44 +18,58 @@ const compare = (a, b) => {
 }
 
 export default function Index () {
-  const [text, setText] = React.useState('')
-  const [selected, setSelected] = React.useState([])
+  const [text, setText] = React.useState('cell')
   const recipes = useLoaderData()
-  const matches = recipes.filter(recipe => compare(recipe.item, text))
+  const [selected, setSelected] = React.useState([
+    {
+      id: nanoid(),
+      ..._.find(recipes, { name: 'Terraspark Boots' })
+    },
+    {
+      id: nanoid(),
+      ..._.find(recipes, { name: 'Cell Phone' })
+    }
+  ])
+  let matches = recipes.filter(recipe => compare(recipe.name, text))
+  matches = _.map(_.groupBy(matches, 'name'), a => a[0])
   if (matches.length > 15) matches.length = 15
   return (
     <div>
-      <h1>Terraria Recipes</h1>
-      <label>Search</label>
-      <input
-        type='search'
-        className='border border-neutral-400 rounded py-2 px-3'
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      {!!matches.length && (
-        <ul className='border'>
-          {matches.map(match => (
-            <li
-              onClick={() =>
-                setSelected(a => [...a, { ...match, id: nanoid() }])
-              }
-            >
-              {match.item}
-            </li>
-          ))}
-        </ul>
-      )}
-      <h1>Selected</h1>
-      <ul>
+      <h1 className='text-3xl text-center py-3'>Terraria Recipes</h1>
+      <div className='mx-8 mb-4'>
+        <input
+          type='search'
+          placeholder='Search...'
+          className='border border-neutral-400 rounded py-2 px-3 w-full'
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        {!!matches.length && (
+          <ul className='shadow-md shadow-slate-300 py-2'>
+            {matches.map(match => (
+              <li
+                key={match.name}
+                className='px-4 py-2 hover:bg-slate-100 cursor-pointer'
+                onClick={() => {
+                  setSelected(a => [...a, { ...match, id: nanoid() }])
+                  setText('')
+                }}
+              >
+                {match.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <ul className='pl-8'>
         {selected.map(item => (
           <Item
+            key={item.id}
             recipes={recipes}
             item={item}
             amount={1}
-            remove={() => {
-              setSelected(a => _.filter(a, a => a.id !== item.id))
-            }}
+            remove={() => setSelected(a => _.reject(a, { id: item.id }))}
+            seen={[]}
           />
         ))}
       </ul>
@@ -62,35 +77,61 @@ export default function Index () {
   )
 }
 
-function Item ({ item, amount, remove, recipes }) {
+function Item ({ item, amount, remove, recipes, seen, parentCompleted }) {
+  const [selfCompleted, setCompleted] = React.useState(false)
+  const completed = selfCompleted || parentCompleted
+  const entries = _.filter(recipes, { name: item.name })
   return (
-    <li key={item.id} className='pl-3 ml-3 list-disc'>
-      <label>
+    <li
+      className={clsx('list-disc', {
+        'line-through text-gray-300': completed,
+        'border-2 border-emerald-400 inline-block p-4 align-top m-3 relative': remove
+      })}
+    >
+      <label className={clsx({ 'pr-10': remove })}>
         <span>
-          {item.item} (made at {item.workbench.join('/')})
+          <input
+            type='checkbox'
+            className='mr-1'
+            checked={completed}
+            onChange={e => setCompleted(e.target.checked)}
+          />
+          <span>{item.name}</span>
+          {amount !== 1 && <span> ({amount})</span>}
+          {item.workbench && <span> ({item.workbench.join('/')})</span>}
+          {entries.length >= 2 && (
+            <button className='text-blue-500 text-sm ml-2'>change</button>
+          )}
         </span>
         {remove && (
-          <button className='border px-3 py-2 ml-2' onClick={remove}>
-            X
+          <button
+            className='px-3 ml-2 text-xl bg-red-400 hover:bg-red-500 active:bg-red-600 text-white rounded absolute top-3 right-3'
+            onClick={remove}
+          >
+            ×
           </button>
         )}
       </label>
-      <ul>
-        {item.ingredients.map(ingredient => {
-          const thing = _.find(recipes, { item: ingredient.item })
-          if (thing) {
+      {item.ingredients && (
+        <ul className='pl-4'>
+          {item.ingredients.map(ingredient => {
+            if (ingredient.name.includes(' Wall')) return null
+            if (seen.includes(ingredient.name)) return null
+            let thing = _.find(recipes, { name: ingredient.name })
+            if (!thing) thing = { name: ingredient.name }
             return (
-              <Item item={thing} amount={ingredient.amount} recipes={recipes} />
+              <Item
+                key={thing.name}
+                item={thing}
+                amount={ingredient.amount}
+                recipes={recipes}
+                seen={[...seen, thing.name]}
+                parentCompleted={completed}
+              />
             )
-          }
-          return (
-            <li className='pl-3 ml-3 list-disc'>
-              {ingredient.item}
-              {ingredient.amount !== 1 && ` (${ingredient.amount})`}
-            </li>
-          )
-        })}
-      </ul>
+          })}
+        </ul>
+      )}
     </li>
   )
 }
